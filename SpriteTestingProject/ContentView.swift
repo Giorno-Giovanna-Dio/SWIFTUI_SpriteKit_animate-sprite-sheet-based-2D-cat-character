6bf3631994f9361da31a805654bbbde62fd83731
Giorno-Giovanna-Dio <dcetpn@gmail.com>
2025-11-07 14:47:20 +0800

    Initial Commit

---
+++ SpriteTestingProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpriteTestingProject
+//
+//  Created by David Chung on 2025/11/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpriteTestingProject/SpriteTestingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteTestingProjectApp.swift
+//  SpriteTestingProject
+//
+//  Created by David Chung on 2025/11/7.
+//
+
+import SwiftUI
+
+@main
+struct SpriteTestingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
